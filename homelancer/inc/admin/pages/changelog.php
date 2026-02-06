b2b8800354fb57fe280569dba4a33070f37efb81
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 06:32:38 +0000

    1.0.2

---
+++ homelancer/inc/admin/pages/changelog.php
@@ -0,0 +1,19 @@
+<div class="homelancer-page__changelog">
+	<h2><?php esc_html_e('Changelog', 'homelancer'); ?></h2>
+
+	<div class="version">
+		<h3><?php esc_html_e('Version 1.0.2', 'homelancer'); ?></h3>
+		<p class="date"><?php esc_html_e('Released: October 16, 2025', 'homelancer'); ?></p>
+		<ul class="changes">
+			<li><?php esc_html_e('User permission check in AJAX plugin installation and activation.', 'homelancer'); ?></li>
+		</ul>
+	</div>
+
+	<div class="version">
+		<h3><?php esc_html_e('Version 1.0.1', 'homelancer'); ?></h3>
+		<p class="date"><?php esc_html_e('Released: July 29, 2025', 'homelancer'); ?></p>
+		<ul class="changes">
+			<li><?php esc_html_e('Initial Release', 'homelancer'); ?></li>
+		</ul>
+	</div>
+</div>
